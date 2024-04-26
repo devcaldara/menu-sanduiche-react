fdfe82178fe1869fb2b752c29bde1655beb5a803
pedro-caldara <devcaldara@gmail.com>
2024-04-25 21:37:14 -0300

    1st commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Barras from './Components/Barras/Barras';
+
+function App() {
+  return (
+    <div className="App">
+        <Barras></Barras>
+    </div>
+  );
+}
+
+export default App;
